f6e6dfe13e980001b73b55e674c0b8f5fb5f914c
vijaykmittalmu <vijaykmittalmu@gmail.com>
2022-12-09 14:21:47 +0530

    unwanted comment remove

--- routes/users.js
+++ routes/users.js
@@ -5,16 +5,9 @@ import { users } from "../controller/users";
 
 const router = express.Router();
 
-// get all users list api
 router.get("/", users.allUsers);
-
-// get specific user information api
 router.get("/:id", users.specificUserDetails);
-
-// update the user information
 router.put("/:id", users.updateSpecificUserDetails);
-
-// delete user
 router.delete("/:id", admin, users.deleteUser);
 
 export default router;
